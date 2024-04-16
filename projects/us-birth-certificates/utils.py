b87e92dad852d20ff2d4070b4176e5af74dce747
Frank Buckley <frank@frankbuckley.com>
2024-04-16 18:44:27 +0100

    Correct lb chance formula

--- projects/us-birth-certificates/utils.py
+++ projects/us-birth-certificates/utils.py
@@ -6,7 +6,9 @@ import math
 def get_ds_lb_chance(y: int) -> float:
     """Returns the chance of Down syndrome given the mother's age.
 
-    Ref: https://doi.org/10.1136/jms.9.1.2
+    Refs:
+    - https://doi.org/10.1136/jms.9.1.2
+    - https://doi.org/10.1258/096914105775220679 (corrected formula)
     """
 
-    return 1/(1+math.exp(7.33-4.211/(1+math.exp(-0.282*(y-37.23)))))
+    return 1/(1+math.exp(7.33-4.211/(1+math.exp(-0.2815*(y-37.23)))))
